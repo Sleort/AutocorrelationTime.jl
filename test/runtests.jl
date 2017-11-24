8a22dbfba1447c9e08b4142d4ec5a6a22e356226
Unknown <tr-ab@online.no>
2017-11-24 17:00:58 +0900

    Test for uncorrelated dataseries (random numbers)
    Test passed

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,4 @@ using AutocorrelationTime
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@test 0.4 < integrated_autocorrelation_time(rand(100000)) < 0.6 #Random numbers have integrated autocorrelation time 1/2
